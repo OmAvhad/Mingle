d2f178714162676554683733c2917e9a126c24db
Kolwankar-Siddhiraj <siddhirajk77@gmail.com>
2023-03-05 04:22:11 +0530

    updated models

--- event/models.py
+++ event/models.py
@@ -29,8 +29,8 @@ class Image(models.Model):
 
 class Icebreakers(models.Model):
     value = models.CharField(max_length=200,null=True,blank=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=False)
+    created_at = models.DateTimeField(auto_now=True,null=True,blank=True)
+    updated_at = models.DateTimeField(auto_now_add=False,null=True,blank=True)
 
 
 
