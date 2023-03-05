d7939c1338ac2a62ab37345e191e8afc46c84219
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2023-03-05 09:21:15 +0530

    added create event

--- event/urls.py
+++ event/urls.py
@@ -3,12 +3,11 @@ from django.urls import *
 from .views import *
 
 urlpatterns = [
-    path('add-event/',addEvent),
+    path('org/create-event/',addEvent),
     path('get-event-list/',getEventList),
     path('add-rating/',addRating),
     path('get-event/',getEvent),
     path('get-event-list-org/',getEventListOrg),
     path('register-for-event/',registerForEvent),
-    path('get-register-data/',getRegisterData), 
-    path('org/create-event/',create_event)
+    path('get-register-data/',getRegisterData),
 ]
